86abd5bd9e886975fef8ea0b5b0ae88e8aef9b1a
m-needham <m.needham@colostate.edu>
2022-11-07 14:03:23 -0700

    added verbose flag to call to _generate_casenames.py

--- src/submit.sh
+++ src/submit.sh
@@ -70,7 +70,7 @@ VERBOSE="20"
 # -----PERFORM ANALYSIS WITH PYTHON SCRIPTS------------------------------------
 
 # 1. GENERATE A LIST OF CASENAMES FROM THE SPECIFIED ENSEMBLE
-python3 _generate_casenames.py --casenames_file $CASENAMES_FILE --data_freq $DATA_FREQ --ensemble_name $ENSEMBLE_NAME --use_provided_casenames $USE_PROVIDED_CASENAMES
+python3 _generate_casenames.py --casenames_file $CASENAMES_FILE --data_freq $DATA_FREQ --ensemble_name $ENSEMBLE_NAME --use_provided_casenames $USE_PROVIDED_CASENAMES --verbose $VERBOSE
 
 # 2. PERFORM THE PRIMARY DATA ANALYSIS
 python3 _ensemble_analysis.py --casenames_file $CASENAMES_FILE --concat_results $CONCAT_RESULTS --data_freq $DATA_FREQ --ensemble_name $ENSEMBLE_NAME --job_scheduler $JOB_SCHEDULER --nc_file_timestr $NC_FILE_TIMESTR --parallel $PARALLEL --preprocess_kwargs $PREPROCESS_KWARGS --save_path $SAVE_PATH --save_name $SAVE_NAME --skip_analysis $SKIP_ANALYSIS --skip_preprocess $SKIP_PREPROCESS --testing_mode_n_ens $TESTING_MODE_N_ENS --testing_mode_n_time $TESTING_MODE_N_TIME --user $USER --verbose $VERBOSE 
