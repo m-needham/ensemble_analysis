88e13dc32fd0745b68ae82f2db1f58a64372a90d
m-needham <m.needham@colostate.edu>
2022-11-08 13:20:42 -0700

    changed create_project.sh script to copy user_functions_test_set.py and rename to user_functions.py in the new directory

--- create_project.sh
+++ create_project.sh
@@ -12,8 +12,10 @@ mkdir -p $DIRECTORY_NAME
 
 cd $DIRECTORY_NAME
 
-cp ../src/_user_functions.py ./
+cp ../src/_user_functions_test_set.py ./
 cp ../src/submit.sh ./
+
+mv _user_functions_test_set.py _user_functions.py
 
 sed -i -e "s/brightness_temperature/$DIRECTORY_NAME/" submit.sh
 
